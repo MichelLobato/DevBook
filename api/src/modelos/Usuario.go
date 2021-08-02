ff9ca9c9beeb1b8a59347ad703f6fe35fe1748d9
Michel Lobato <michelg.lobato@live>
2021-08-02 18:20:03 -0300

    Terceiro commit "Hash na senha e comparacao de senha"

--- api/src/modelos/Usuario.go
+++ api/src/modelos/Usuario.go
@@ -1,9 +1,12 @@
 package modelos
 
 import (
+	"api/src/seguranca"
 	"errors"
 	"strings"
 	"time"
+
+	"github.com/badoux/checkmail"
 )
 
 // Usuario representa um usuario utilizando a rede social
@@ -22,7 +25,10 @@ func (usuario *Usuario) Preparar(etapa string) error {
 		return erro
 	}
 
-	usuario.formatar()
+	if erro := usuario.formatar(etapa); erro != nil {
+		return erro
+	}
+
 	return nil
 }
 
@@ -39,6 +45,10 @@ func (usuario *Usuario) validar(etapa string) error {
 		return errors.New("O email e um campo obrigatorio nao pode estar em branco")
 	}
 
+	if erro := checkmail.ValidateFormat(usuario.Email); erro != nil {
+		return errors.New("O e-mail inserido e invalido")
+	}
+
 	if etapa == "cadastro" && usuario.Senha == "" {
 		return errors.New("O senha e um campo obrigatorio nao pode estar em branco")
 	}
@@ -46,8 +56,19 @@ func (usuario *Usuario) validar(etapa string) error {
 	return nil
 }
 
-func (usuario *Usuario) formatar() {
+func (usuario *Usuario) formatar(etapa string) error {
 	usuario.Nome = strings.TrimSpace(usuario.Nome)
 	usuario.Nick = strings.TrimSpace(usuario.Nick)
 	usuario.Email = strings.TrimSpace(usuario.Email)
+
+	if etapa == "cadastro" {
+		senhaComHash, erro := seguranca.Hash(usuario.Senha)
+		if erro != nil {
+			return erro
+		}
+
+		usuario.Senha = string(senhaComHash)
+	}
+
+	return nil
 }
